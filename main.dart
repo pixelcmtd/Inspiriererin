bc2633c2abd36ddf5e2e32200eb00daf3e39571a
pixel <chrissx@chrissx.de>
2021-09-25 15:38:05 +0200

    lets just await creating the reactions
    not doing so is actually a very bad idea it seems

--- main.dart
+++ main.dart
@@ -6,9 +6,9 @@ final api = Uri.parse('https://inspirobot.me/api?generate=true');
 Future<void> inspire(TextChannel c, String u, int d) async {
   final url = (await http.get(api)).body;
   final r = await c.sendMessage(MessageBuilder.content(url));
-  r.createReaction(UnicodeEmoji('⏭'));
-  r.createReaction(UnicodeEmoji('🔼️'));
-  r.createReaction(UnicodeEmoji('🔽'));
+  await r.createReaction(UnicodeEmoji('⏭'));
+  await r.createReaction(UnicodeEmoji('🔼️'));
+  await r.createReaction(UnicodeEmoji('🔽'));
   print('Inspired $u#$d at ${r.url}: $url');
 }
 
